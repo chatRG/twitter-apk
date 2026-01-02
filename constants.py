37c0d6b1318a1954f67ed56a0cab0530ccb27e26
chatRG <sayan22c@gmail.com>
2026-01-02 21:42:44 +0530

    Change user-agent string in constants.py

--- constants.py
+++ constants.py
@@ -13,5 +13,5 @@ HEADERS = {
     "sec-fetch-site": "none",
     "sec-fetch-user": "?1",
     "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+    "user-agent": "APKUpdater-v1.0",
 }
